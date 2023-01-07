30f8cccc4f898d599930272a4e9b940fc7041462
Sharpy <89833843+sharpyanand69@users.noreply.github.com>
2023-01-07 12:42:28 +0000

    Registration modal done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  onClick(){
+
   }
 
 }
