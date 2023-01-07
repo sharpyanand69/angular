38a25b0fe9e8b492dc0ad4eb5d3fd85b9ca75339
Sharpy <89833843+sharpyanand69@users.noreply.github.com>
2023-01-07 11:03:29 +0000

    poject sanctified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ButtonComponent,
+    AppComponent
   ],
-  imports: [
+  imports: [                                      
     BrowserModule,
     AppRoutingModule
   ],
